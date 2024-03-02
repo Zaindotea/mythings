a496657071bf6cd3b0b7b0abcd5257bf679ea22c
Zaindotea <vmarcelo2004@gmail.com>
2024-03-01 22:21:39 -0300

    Added content to style an script

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-ECHO is on.
+var aniversary = new Date("26 Nov, 2021");
+var now = new Date();
+var difftime = now - aniversary;
+var diffdays = Math.round(difftime / (1000 * 24 * 60 * 60));
+document.getElementById("time").innerHTML += `${diffdays}`;
